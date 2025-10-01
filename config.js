ccf9f2172eb566a9f8ba2109f24bc3bce0e9484d
teamtangaco-code <teamtanga.co@gmail.com>
2025-10-01 22:59:45 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window.__firebase_config = JSON.stringify({
+  apiKey: "AIzaSyAY2GPRaHbU7vfA8J79mewyo2u6qHr-a6s",
+  authDomain: "buobuo-ka-81320.firebaseapp.com",
+  projectId: "buobuo-ka-81320",
+  storageBucket: "buobuo-ka-81320.appspot.com",
+  messagingSenderId: "687871046289",
+  appId: "1:687871046289:web:e8efb15aa500add7164530"
+});
